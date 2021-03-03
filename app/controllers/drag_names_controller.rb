6adba84e808549e736549bb6304b1f3f1aa658b7
jason1110 <Jason.easterly11@gmail.com>
2021-03-03 09:21:37 -0700

    add controller methods

--- app/controllers/drag_names_controller.rb
+++ app/controllers/drag_names_controller.rb
@@ -1,2 +1,31 @@
 class DragNamesController < ApplicationController
+
+    def index
+        @all_drag_names = DragName.all
+
+        render json: @all_drag_names
+    end
+
+    def show
+        @drag_name = DragName.find9params[:id])
+
+        render json: @drag_name
+    end
+
+    def create
+        @new_drag_names = DragName.create(
+            first_name: params[:first_name],
+            last_name: param[:last_name]
+        )
+        render json: @new_drag_names, status: :created
+    end
+
+    def destroy
+        @drag_name = DragName.find(params[:id])
+
+        @drag_name.destroy
+        
+        render json: "Drag name has been deleted"
+    end
+
 end
